09e1129bd5036e36fdf1e3edb26b0a156004f77a
daisymayorga-galtechs <68982348+daisymayorga-galtechs@users.noreply.github.com>
2025-05-04 20:02:37 -0700

    Added title and description

--- exponents.py
+++ exponents.py
@@ -1,6 +1,10 @@
 # streamlit_app.py
 import streamlit as st
 
+st.title("Exponents Explained: Math Magic for Repeating Multiplication!")
+st.markdown("Think of exponents like a superhero shortcut in math. Instead of writing: 2 × 2 × 2 we just write: 2³. The small number (the 3) tells you how many times to multiply the big number (the 2) by itself. So, 2³ = 2 × 2 × 2 = 8 It’s like saying, “Hey 2, multiply yourself three times in a row!” ")
+
+
 base = st.number_input("Enter a base:", value=2)
 expo = st.number_input("Enter an exponent:", value=3)
 result = base ** expo
